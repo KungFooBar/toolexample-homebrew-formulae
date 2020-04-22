7ac27b84aadbfeeedf7aaee3fccab85e4519f56e
Dmitry <belousow.dmitri@gmail.com>
2020-04-22 17:26:50 +0300

    Update toolexample.rb

--- Formula/toolexample.rb
+++ Formula/toolexample.rb
@@ -4,9 +4,7 @@ class Toolexample < Formula
   url "https://github.com/KungFooBar/ToolExample/archive/v1.0.0.tar.gz"
 
   def install
-  	system "swift", "build",
-  		"-c", "release",
-  		"--disable-sandbox"
+  	system "swift", "build", "-c", "release", "--disable-sandbox"
   	bin.install "./build/release/toolexample"
   end
 
