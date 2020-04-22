f4dc8a41fe244266f17af9b20203f883da032029
Дмитрий Белоусов <belousow.dmitri@gmail.com>
2020-04-22 17:07:12 +0300

    Initial commit

---
+++ Formula/toolexample.rb
@@ -0,0 +1,13 @@
+class Toolexample < Formula
+  desc "ToolExample"
+  homepage "https://github.com/KungFooBar/ToolExample"
+  url "https://github.com/KungFooBar/ToolExample/archive/v1.0.0.tar.gz"
+
+  def install
+  	system "swift", "build",
+  		"-c", "release",
+  		"--disable-sandbox"
+  	bin.install "./build/release/toolexample"
+  end
+
+end
